c0dfc9dc3f329d0b5d56f286217cf3a22c4721da
rafael ribas <rafacribas@gmail.com>
2023-08-19 13:27:21 -0300

    fix response

--- config/plugins.js
+++ config/plugins.js
@@ -1,21 +1,40 @@
 module.exports = ({ env }) => ({
-    'users-permissions': {
-      config: {
+  'users-permissions': {
+    config: {
       jwtSecret: env('JWT_SECRET'),
+    },
+  },
+  'transformer': {
+    enabled: true,
+    config: {
+      responseTransforms: {
+        removeAttributesKey: true,
+        removeDataKey: true,
+      },
+      requestTransforms: {
+        wrapBodyWithDataKey: true
+      },
+      hooks: {
+        preResponseTransform: (ctx) => console.log('hello from the preResponseTransform hook!'),
+        postResponseTransform: (ctx) => console.log('hello from the postResponseTransform hook!')
+      },
+      contentTypeFilter: {
+        mode: 'allow'
+      }
+    }
+  },
+  upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
       },
     },
-    upload: {
-      config: {
-        provider: 'cloudinary',
-        providerOptions: {
-          cloud_name: env('CLOUDINARY_NAME'),
-          api_key: env('CLOUDINARY_KEY'),
-          api_secret: env('CLOUDINARY_SECRET'),
-        },
-        actionOptions: {
-          upload: {},
-          delete: {},
-        },
-      },
-    },
-  });
+  },
+});
